06f2cae6605a4f27bfaa67c83e088f5e8e86f235
kananthav <kevin.anantha@gmail.com>
2018-09-26 14:03:27 +0800

    Delete post

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Post extends Component {
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+    this.props.history.push('/');
+  }
   render() {
+  console.log(this.props);
     const post = this.props.post ? (
       <div className="center">
         <h4 className="post">{ this.props.post.title }</h4>
         <p>{ this.props.post.body }</p>
+        <div className="center">
+          <button className="btn red" onClick={this.handleClick}>
+            Delete post
+          </button>
+        </div>
       </div>
     ) : (
       <div className="center">Loading post...</div>
@@ -27,4 +37,10 @@ const mapStateToProps = (state, ownProps) => {
   }
 };
 
-export default connect(mapStateToProps)(Post);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deletePost: (id) => { dispatch({type: 'DELETE_POST', id: id}) }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post);
